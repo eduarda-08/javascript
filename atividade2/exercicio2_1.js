c075bfe26b95e16c62c6482ccd18bc233b1c5e3b
eduarda-08 <eduarda.paula.fagundes@escola.pr.gov.br>
2025-02-19 09:54:57 -0300

    Add files via upload

---
+++ atividade2/exercicio2_1.js
@@ -0,0 +1,9 @@
+function inverterString(texto) {
+    return texto.split('').reverse().join('');
+}
+
+let palavra = "dudona";
+let palavraInvertida = inverterString(palavra);
+
+console.log(palavraInvertida); 
+
